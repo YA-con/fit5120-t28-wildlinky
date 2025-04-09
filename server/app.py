dc62b6e72716852b33294ea86d07fc14ff5364a5
YA-con <yzhanglio233@gmail.com>
2025-04-09 13:12:49 +1000

    fix: import os to support dynamic port binding

--- server/app.py
+++ server/app.py
@@ -255,4 +255,4 @@ def initialize_server_data():
 if __name__ == '__main__':
     initialize_server_data()
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=port)
